4166bbbe14eaf5dc6c49d370d9e0c0ec82bad688
jaemjeon <piepie1346@gmail.com>
2024-11-29 07:14:22 +0900

    add treasure_image samples update resources path

--- Assets/Scripts/env.cs
+++ Assets/Scripts/env.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 static class Constants
 {
-    public const string DESCRIPTIONS_PATH = "Assets/Descriptions/";
+    public const string DESCRIPTIONS_PATH = "Assets/Resources/Descriptions/";
+    public const string TREASURE_IMAGE_PATH = "Assets/Resources/TreasureImages/";
     public const int BACKGROUND_SOUND_INIT_VALUE = 100;
 }
