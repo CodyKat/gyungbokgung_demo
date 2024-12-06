c2f5d7c01bce1939022ee2adf4001bf7a758ba15
dlwngjs <dlwngjs1227@naver.com>
2024-12-06 18:29:33 +0900

    발걸음 소리 2차 테스트

--- Assets/WalkSound.cs
+++ Assets/WalkSound.cs
@@ -1,23 +1,65 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class WalkSound : MonoBehaviour
 {
+    public InputDeviceCharacteristics controllerCharacteristics;
+    public List<GameObject> controllerPrefabs;
+    private InputDevice targetDevice;
+    private GameObject spawnedController;
     private AudioSource audioSource;
-    private bool IsMoving;
 
     void Start()
     {
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesWithCharacteristics(controllerCharacteristics, devices);
         audioSource = gameObject.GetComponent<AudioSource>();
+
+
+        //input devices üũ debug
+        foreach (var item in devices)
+        {
+            Debug.Log(item.name + item.characteristics);
+        }
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+            //targetDevice�� �̸��� ������ controller�� ã�´�. 
+            GameObject prefab = controllerPrefabs.Find(controller => controller.name == targetDevice.name);
+
+            if (prefab) //prefab�� ������ �� 
+            {
+                spawnedController = Instantiate(prefab, transform);
+                Debug.Log(prefab);
+            }
+            else //prefab�� controller�� ã�� ���� ��
+            {
+                Debug.LogError("Did not find corresponding controller model.");
+                spawnedController = Instantiate(controllerPrefabs[0], transform);
+            }
+        }
     }
 
     void Update()
     {
-        if (Input.GetAxis("Vertical") < 0) IsMoving = true; // better use != 0 here for both directions
-        else IsMoving = false;
+        //input�� debug
+        // if (targetDevice.TryGetFeatureValue(CommonUsages.primaryButton, out bool primaryButtonValue) &&
+        //     primaryButtonValue)
+        //     Debug.Log("Pressing Primary Button");
 
-        if (IsMoving && !audioSource.isPlaying) audioSource.Play(); // if player is moving and audiosource is not playing play it
-        if (!IsMoving) audioSource.Stop(); // if player is not moving and audiosource is playing stop it
+        // if (targetDevice.TryGetFeatureValue(CommonUsages.trigger, out float triggerValue) &&
+        //     triggerValue > 0.1f)
+        //     Debug.Log("Triggerr pressed" + triggerValue);
+
+        if (targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue) && primary2DAxisValue != Vector2.zero){
+            audioSource.Play();
+        }
+        else {
+            audioSource.Stop();
+        }
+    
     }
 }
